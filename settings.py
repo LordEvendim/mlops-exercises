2868034e568771aff11168ad68d0dc9d726d5465
Lord_Evendim <24799163+LordEvendim@users.noreply.github.com>
2025-10-08 09:09:25 +0200

    chore: add envs

---
+++ settings.py
@@ -0,0 +1,18 @@
+# settings.py
+from pydantic_settings import BaseSettings
+from pydantic import field_validator
+
+
+class Settings(BaseSettings):
+    ENVIRONMENT: str
+    APP_NAME: str
+
+    @field_validator("ENVIRONMENT")
+    @classmethod
+    def validate_environment(cls, value):
+        #    ... # implement me!
+        # prepare validator that will check whether the value of ENVIRONMENT is in (dev, test, prod)
+        if value not in ("dev", "test", "prod"):
+            raise ValueError("ENVIRONMENT not found in dev, test or prod")
+
+        return value
